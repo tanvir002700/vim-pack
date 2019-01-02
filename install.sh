0c6c1dec9a510a9a3a217bb97f4e706de210b857
tanvir <tanvir002700@gmail.com>
2019-01-02 13:45:40 +0600

    add pynvim install

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #/usr/bin/env bash
 
 source ./recipes/neovim.sh
+
+install() {
+    if ! which pip > /dev/null && ! which pip3 >/dev/null; then
+        echo "pip not installed...."
+        return
+    fi
+    install_neovim
+}
+
+install
--- recipes/neovim.sh
+++ recipes/neovim.sh
@@ -1,21 +1,23 @@
 #/usr/bin/env bash
 
-if which nvim > /dev/null; then
-    echo "===> Neovim already installed."
-else
-    echo "---> Installing neovim........."
-    echo -ne "---> Installing ===>"
-    sudo apt-get install software-properties-common &> /dev/null
-    echo -ne \\b"=====>"
-    sudo add-apt-repository --remove -y ppa:neovim-ppa/unstable &> /dev/null
-    echo -ne \\b"======>"
-    sudo add-apt-repository --remove -y ppa:neovim-ppa/stable &> /dev/null
-    echo -ne \\b"========>"
-    sudo add-apt-repository -y ppa:neovim-ppa/stable &> /dev/null
-    echo -ne \\b"==========>"
-    sudo apt-get update &> /dev/null
-    echo -ne \\b"============>"
-    sudo apt-get install -y neovim &>> /dev/null
-    echo -ne \\b"==============>"
-    echo
-fi
+install_neovim() {
+    if which nvim > /dev/null; then
+        echo "===> Neovim already installed."
+    else
+        echo "---> Installing neovim........."
+        echo -ne "---> Installing ===>"
+        sudo apt-get install software-properties-common &> /dev/null
+        echo -ne \\b"=====>"
+        sudo add-apt-repository --remove -y ppa:neovim-ppa/unstable &> /dev/null
+        echo -ne \\b"======>"
+        sudo add-apt-repository --remove -y ppa:neovim-ppa/stable &> /dev/null
+        echo -ne \\b"========>"
+        sudo add-apt-repository -y ppa:neovim-ppa/stable &> /dev/null
+        echo -ne \\b"==========>"
+        sudo apt-get update &> /dev/null
+        echo -ne \\b"============>"
+        sudo apt-get install -y neovim &>> /dev/null
+        echo -ne \\b"==============>"
+        echo
+    fi
+}
