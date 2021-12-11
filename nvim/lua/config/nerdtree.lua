273beb351cf21ac9aca5319e7f9444f2929ba604
Tanvir Hasan <tanvir002700@gmail.com>
2021-12-11 23:33:26 +0100

    remove unused config

--- nvim/lua/config/nerdtree.lua
+++
@@ -1,7 +0,0 @@
-vim.g.NERDTreeWinSize = 25
-vim.g.NERDTreeHijackNetrw = false
-vim.g.NERDTreeShowHidden = true
-vim.g.NERDTreeIgnore = {'^__pycache__$', '^.venv$', '^.git$', '^tags$', '^tag$', '^node_modules$'}
-
-vim.api.nvim_set_keymap('n', '<F6>', ':NERDTreeToggle<CR>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<F5>', ':NERDTreeFind<CR>', {noremap=true})
